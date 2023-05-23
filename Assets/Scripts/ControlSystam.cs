87969c439e1bddeb20c1c04b9522d36b498f5642
mikururu <airantou1996413@gmail.com>
2023-05-23 21:34:11 +0800

    Update_控制系統

---
+++ Assets/Scripts/ControlSystam.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ControlSystam : MonoBehaviour
+{
+	private void Awake()
+	{
+		print("123");
+	}
+	private void Start()
+	{
+		print("<color=#f00>開始</color>");
+	}
+	private void Update()
+	{
+		print("<color=yellow>更新</color>");
+	}
+}
---
+++ Assets/Scripts/MenuMenager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuMenager : MonoBehaviour
+{
+	public void StartGame()
+	{
+		SceneManager.LoadScene("遊戲場景");
+	}
+	public void QuitGame() 
+	{
+		Application.Quit();
+	}
+}
