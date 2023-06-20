4d8b54488b0d131312a88def8e7a38d38190b89f
mikururu <airantou1996413@gmail.com>
2023-06-20 21:46:09 +0800

    Update_無敵bluebird

---
+++ Assets/Scripts/DamageBasic.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class DamageBasic : MonoBehaviour
+{
+	[Header("資料")]
+	public DataBasic data;
+
+	private float hp;
+
+	private void Awake()
+	{
+		hp = data.hp;
+	}
+
+	public void Damage(float damage)
+	{
+		hp -= damage;
+		print($"<color=#fe6>{gameObject.name} 血量剩下:{hp}</color>");
+
+		if (hp <= 0) Dead();
+	}
+	private void Dead()
+	{
+		print($"<color=#f96>{gameObject.name} 死亡</color>");
+	}
+}
