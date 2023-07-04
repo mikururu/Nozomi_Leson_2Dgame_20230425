9a4cc363a52efe6ff805f2dda414b09bdadb15a2
mikururu <airantou1996413@gmail.com>
2023-07-04 21:32:50 +0800

    update_經驗值

--- Assets/Scripts/DamageBasic.cs
+++ Assets/Scripts/DamageBasic.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class DamageBasic : MonoBehaviour
 {
 	[Header("資料")]
 	public DataBasic data;
+	[Header("傷害值欲=預置物")]
+	public GameObject prefabDamage;
 
 	private float hp;
 
@@ -15,11 +18,17 @@ public class DamageBasic : MonoBehaviour
 	public void Damage(float damage)
 	{
 		hp -= damage;
+
+		GameObject tempDamage = Instantiate(prefabDamage, transform.position, transform.rotation);
+		tempDamage.transform.Find("傷害值文字").GetComponent<TextMeshProUGUI>().text = damage.ToString();
+
+		Destroy(tempDamage, 1.5f);
+
 		print($"<color=#fe6>{gameObject.name} 血量剩下:{hp}</color>");
 
 		if (hp <= 0) Dead();
 	}
-	private void Dead()
+	protected virtual void Dead()
 	{
 		print($"<color=#f96>{gameObject.name} 死亡</color>");
 	}
--- Assets/Scripts/DamageEnemy.cs
+++ Assets/Scripts/DamageEnemy.cs
@@ -2,14 +2,33 @@
 
 public class DamageEnemy : DamageBasic 
 {
-	[Header("傷害值欲=預置物")]
-	public GameObject prefabDamage;
+	private DataEnemy dataEnemy;
+
+	private void Start()
+	{
+		dataEnemy = (DataEnemy)data;
+		print(dataEnemy.expProbability);
+	}
+
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
 		if (collision.gameObject.name.Contains("武器"))
 		{
 			Damage(50);
-			Instantiate(prefabDamage,transform.position,transform.rotation);
+			
+		}
+	}
+
+	protected override void Dead()
+	{
+		base.Dead();
+		Destroy(gameObject);
+
+		//print("隨機值:" + Random.value);
+
+		if (Random.value < dataEnemy.expProbability)
+		{
+			Instantiate(dataEnemy.prefabExp, transform.position, transform.rotation);
 		}
 	}
 }
